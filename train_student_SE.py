124b15b1aa365a8b0a6e999db6c7c9b2c58339a5
ejcrowley <elliotjcrowley@gmail.com>
2017-10-10 14:59:57 +0100

    SE style loss

--- train_student_SE.py
+++ train_student_SE.py
@@ -130,7 +130,7 @@ checkpoint = torch.load('checkpoints/%s.t7' % args.teacher_checkpoint)
 teach = checkpoint['net']
 
 print ('==> Loaded teacher..')
-se_loss
+
 teach = teach.cuda()
 
 for param in teach.parameters():
@@ -152,12 +152,11 @@ def distillation(y, teacher_scores, labels, T, alpha):
            + F.cross_entropy(y, labels) * (1. - alpha)
 
 
-def at(x):
-    return F.normalize(x.pow(2).mean(1).view(x.size(0), -1))
+def se(x):
+    return F.normalize(x.pow(2).mean(-1).mean(-1).view(x.size(0), -1))
 
-
-def at_loss(x, y):
-    return (at(x) - at(y)).pow(2).mean()
+def se_loss(x, y):
+    return (se(x) - se(y)).pow(2).mean()
 
 
 def l1_loss(x):
@@ -183,7 +182,7 @@ def train(epoch):
 
         #Add an attention tranfer loss for each intermediate.
         for i in range(len(ints_student)):
-            loss += args.beta * at_loss(ints_student[i], ints_teacher[i])
+            loss += args.beta * se_loss(ints_student[i], ints_teacher[i])
 
         optimizer.zero_grad()
         loss.backward()
