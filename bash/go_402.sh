68009f95b7bdf3c056434c7ec0b5ca41e6e87d87
ejcrowley <elliotjcrowley@gmail.com>
2017-10-10 15:48:01 +0100

    modified bash

--- bash/go_402.sh
+++ bash/go_402.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-python train_student_KD.py WRN 3    -t wrn_40_2 -s wrn_$1_$2_3x3_KD_w_40_2 --wrn_depth $1 --wrn_width $2 --GPU $3
-python train_student_KD.py WRN2x2 3 -t wrn_40_2 -s wrn_$1_$2_2x2_KD_w_40_2 --wrn_depth $1 --wrn_width $2 --GPU $3
-python train_student_KD.py WRNsep 3 -t wrn_40_2 -s wrn_$1_$2_sep_KD_w_40_2 --wrn_depth $1 --wrn_width $2 --GPU $3
+python train_student_KD.py WRN     -t wrn_40_2 -s wrn_$1_$2_3x3_KD_w_40_2 --wrn_depth $1 --wrn_width $2 --GPU $3
+python train_student_KD.py WRN2x2  -t wrn_40_2 -s wrn_$1_$2_2x2_KD_w_40_2 --wrn_depth $1 --wrn_width $2 --GPU $3
+python train_student_KD.py WRNsep  -t wrn_40_2 -s wrn_$1_$2_sep_KD_w_40_2 --wrn_depth $1 --wrn_width $2 --GPU $3
 
 python train_student_AT.py WRN 3    -t wrn_40_2 -s wrn_$1_$2_3x3_AD_w_40_2 --wrn_depth $1 --wrn_width $2 --GPU $3
 python train_student_AT.py WRN2x2 3 -t wrn_40_2 -s wrn_$1_$2_2x2_AD_w_40_2 --wrn_depth $1 --wrn_width $2 --GPU $3
