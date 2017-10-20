5b37d6c61cf8e246f83ef716d11e3d068e801156
ejcrowley <elliotjcrowley@gmail.com>
2017-10-20 16:40:02 +0100

    big bash

--- bash/go_AT_range.sh
+++ bash/go_AT_range.sh
@@ -4,20 +4,20 @@ array=( DConv DConvG2 DConvG4 DConvG8 DConvG16 )
 for i in "${array[@]}"
 do
     tmux \
-      new-session  "python main.py AT $i -s wkAT_$i_split2_40_2 --GPU 0 --wrn_depth 40 --wrn_width 2 --alpha 0 --AT_split 1" \; \
-      split-window "python main.py AT $i -s wkAT_$i_split3_40_2 --GPU 1 --wrn_depth 40 --wrn_width 2 --alpha 0 --AT_split 2"\; \
-      split-window "python main.py AT $i -s wkAT_$i_split3_40_2 --GPU 2 --wrn_depth 40 --wrn_width 2 --alpha 0 --AT_split 3"\; \
-      split-window "python main.py AT $i -s wkAT_$i_split6_40_2 --GPU 3 --wrn_depth 40 --wrn_width 2 --alpha 0 --AT_split 6" \; \
+      new-session  "python main.py AT $i -s wkAT_split2_40_2_$i --GPU 0 --wrn_depth 40 --wrn_width 2 --alpha 0 --AT_split 1" \; \
+      split-window "python main.py AT $i -s wkAT_split3_40_2_$i --GPU 1 --wrn_depth 40 --wrn_width 2 --alpha 0 --AT_split 2"\; \
+      split-window "python main.py AT $i -s wkAT_split3_40_2_$i --GPU 2 --wrn_depth 40 --wrn_width 2 --alpha 0 --AT_split 3"\; \
+      split-window "python main.py AT $i -s wkAT_split6_40_2_$i --GPU 3 --wrn_depth 40 --wrn_width 2 --alpha 0 --AT_split 6" \; \
       select-layout even-vertical
 done
 
 for i in "${array[@]}"
 do
     tmux \
-      new-session  "python main.py AT $i -s wkAT_$i_Bsplit2_40_2 --GPU 0 --wrn_depth 40 --wrn_width 2 --alpha 0 --AT_split 1 --block Bottle" \; \
-      split-window "python main.py AT $i -s wkAT_$i_Bsplit3_40_2 --GPU 1 --wrn_depth 40 --wrn_width 2 --alpha 0 --AT_split 2 --block Bottle"\; \
-      split-window "python main.py AT $i -s wkAT_$i_Bsplit3_40_2 --GPU 2 --wrn_depth 40 --wrn_width 2 --alpha 0 --AT_split 3 --block Bottle"\; \
-      split-window "python main.py AT $i -s wkAT_$i_Bsplit6_40_2 --GPU 3 --wrn_depth 40 --wrn_width 2 --alpha 0 --AT_split 6 --block Bottle" \; \
+      new-session  "python main.py AT $i -s wkAT_Bsplit2_40_2_$i --GPU 0 --wrn_depth 40 --wrn_width 2 --alpha 0 --AT_split 1 --block Bottle" \; \
+      split-window "python main.py AT $i -s wkAT_Bsplit3_40_2_$i --GPU 1 --wrn_depth 40 --wrn_width 2 --alpha 0 --AT_split 2 --block Bottle"\; \
+      split-window "python main.py AT $i -s wkAT_Bsplit3_40_2_$i --GPU 2 --wrn_depth 40 --wrn_width 2 --alpha 0 --AT_split 3 --block Bottle"\; \
+      split-window "python main.py AT $i -s wkAT_Bsplit6_40_2_$i --GPU 3 --wrn_depth 40 --wrn_width 2 --alpha 0 --AT_split 6 --block Bottle" \; \
       select-layout even-vertical
 done
 
