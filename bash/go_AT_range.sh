006f7fa61e1532a686d5c0472fe7ce41282d4336
ejcrowley <elliotjcrowley@gmail.com>
2017-10-23 14:25:28 +0100

    extra bash

--- bash/go_AT_range.sh
+++ bash/go_AT_range.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 # declare an array called array and define 3 vales
-array=( DConv DConvG2 DConvG4 DConvG8 DConvG16 )
+array=( DConvG4 DConvG8 DConvG16 DConv DConvG2 )
 for i in "${array[@]}"
 do
-    tmux \
-      new-session  "python main.py AT $i -s wkAT_split1_40_2_$i --GPU 0 --wrn_depth 40 --wrn_width 2 --alpha 0 --AT_split 1" \; \
-      split-window "python main.py AT $i -s wkAT_split2_40_2_$i --GPU 1 --wrn_depth 40 --wrn_width 2 --alpha 0 --AT_split 2"\; \
-      split-window "python main.py AT $i -s wkAT_split3_40_2_$i --GPU 2 --wrn_depth 40 --wrn_width 2 --alpha 0 --AT_split 3"\; \
-      split-window "python main.py AT $i -s wkAT_split6_40_2_$i --GPU 3 --wrn_depth 40 --wrn_width 2 --alpha 0 --AT_split 6" \; \
+    tmux -f /disk/scratch/ecrowley/.tmux.conf \
+      new-session  "python main.py KD $i -s mondayKD_40_2_$i --GPU 0 --wrn_depth 40 --wrn_width 2" \; \
+      split-window "python main.py AT $i -s mondayAT_split1_40_2_$i --GPU 1 --wrn_depth 40 --wrn_width 2 --alpha 0 --AT_split 1"\; \
+      split-window "python main.py AT $i -s mondayAT_split2_40_2_$i --GPU 2 --wrn_depth 40 --wrn_width 2 --alpha 0 --AT_split 2"\; \
+      split-window "python main.py AT $i -s mondayAT_split3_40_2_$i --GPU 3 --wrn_depth 40 --wrn_width 2 --alpha 0 --AT_split 3" \; \
       select-layout even-vertical
 done
 
 for i in "${array[@]}"
 do
-    tmux \
-      new-session  "python main.py AT $i -s wkAT_Bsplit1_40_2_$i --GPU 0 --wrn_depth 40 --wrn_width 2 --alpha 0 --AT_split 1 --block Bottle" \; \
-      split-window "python main.py AT $i -s wkAT_Bsplit2_40_2_$i --GPU 1 --wrn_depth 40 --wrn_width 2 --alpha 0 --AT_split 2 --block Bottle"\; \
-      split-window "python main.py AT $i -s wkAT_Bsplit3_40_2_$i --GPU 2 --wrn_depth 40 --wrn_width 2 --alpha 0 --AT_split 3 --block Bottle"\; \
-      split-window "python main.py AT $i -s wkAT_Bsplit6_40_2_$i --GPU 3 --wrn_depth 40 --wrn_width 2 --alpha 0 --AT_split 6 --block Bottle" \; \
+    tmux -f /disk/scratch/ecrowley/.tmux.conf \
+      new-session  "python main.py KD $i -s m2ondayKD_40_2_$i --GPU 0 --wrn_depth 40 --wrn_width 2" \; \
+      split-window "python main.py AT $i -s m2ondayAT_split1_40_2_$i --GPU 1 --wrn_depth 40 --wrn_width 2 --alpha 0 --AT_split 1"\; \
+      split-window "python main.py AT $i -s m2ondayAT_split2_40_2_$i --GPU 2 --wrn_depth 40 --wrn_width 2 --alpha 0 --AT_split 2"\; \
+      split-window "python main.py AT $i -s m2ondayAT_split3_40_2_$i --GPU 3 --wrn_depth 40 --wrn_width 2 --alpha 0 --AT_split 3" \; \
       select-layout even-vertical
 done
+
+
+
+
+
+
+
+
+
 
 
