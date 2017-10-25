1e8af910e5b89f57ddb96ccd915b4a4ee53ce320
ejcrowley <elliotjcrowley@gmail.com>
2017-10-25 16:40:17 +0100

    simple script for single gpu experiment

--- bash/go1gpu.sh
+++ bash/go1gpu.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
-for arg; do
-      python main.py cifar100  teacher ${arg} -t 402_100_T_${arg}              --GPU 3
-      python main.py cifar100  AT      ${arg} -s 402_100_AT_${arg} --alpha 0    --GPU 3
-      python main.py cifar100  KD      ${arg} -s 402_100_KD_${arg} --alpha 0.9  --GPU 3
-done
+
+python main.py cifar100  teacher Conv -t 162_100_T_               --GPU 3 --wrn_width 2 --wrn_depth 16
+python main.py cifar100  AT      Conv -s 162_100_AT_ --alpha 0    --GPU 3 --wrn_width 2 --wrn_depth 16
+python main.py cifar100  KD      Conv -s 162_100_KD_ --alpha 0.9  --GPU 3 --wrn_width 2 --wrn_depth 16
+
+
+python main.py cifar100  teacher Conv -t 161_100_T_               --GPU 3 --wrn_width 1 --wrn_depth 16
+python main.py cifar100  AT      Conv -s 161_100_AT_ --alpha 0    --GPU 3 --wrn_width 1 --wrn_depth 16
+python main.py cifar100  KD      Conv -s 161_100_KD_ --alpha 0.9  --GPU 3 --wrn_width 1 --wrn_depth 16
+
+
+python main.py cifar100  teacher Conv -t 401_100_T_               --GPU 3 --wrn_width 1 --wrn_depth 40
+python main.py cifar100  AT      Conv -s 401_100_AT_ --alpha 0    --GPU 3 --wrn_width 1 --wrn_depth 40
+python main.py cifar100  KD      Conv -s 401_100_KD_ --alpha 0.9  --GPU 3 --wrn_width 1 --wrn_depth 40
