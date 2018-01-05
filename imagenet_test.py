6c24ee84719c49260a2ee19c9c121f96a56ac2fd
ejcrowley <elliotjcrowley@gmail.com>
2018-01-05 14:52:37 +0000

    added an imagenet evaluation script

---
+++ imagenet_test.py
@@ -0,0 +1,125 @@
+import torch
+import os
+import torchvision
+import torchvision.transforms as transforms
+import time
+import torch.nn as nn
+import torchvision.models.resnet
+import torch.utils.model_zoo as model_zoo
+from models import *
+
+imagenet_loc = '/disk/scratch_ssd/imagenet'
+
+model = resnet34(pretrained=True)
+
+#model = torchvision.models.resnet.resnet34(pretrained=True)
+model = torch.nn.DataParallel(model).cuda()
+
+workers = 8
+batch_size = 256
+print_freq = 10
+num_classes = 1000
+traindir = os.path.join(imagenet_loc, 'train')
+valdir = os.path.join(imagenet_loc, 'val')
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+
+transform_train = transforms.Compose([
+    transforms.RandomResizedCrop(224),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    normalize,
+])
+transform_test = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    normalize,
+])
+
+trainset = torchvision.datasets.ImageFolder(traindir, transform_train)
+testset = torchvision.datasets.ImageFolder(valdir, transform_test)
+
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True,
+                                          num_workers=workers,
+                                          pin_memory=True)
+testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False,
+                                         num_workers=workers,
+                                         pin_memory=True)
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+        res.append(correct_k.mul_(100.0 / batch_size))
+    return res
+
+def validate(val_loader, model, criterion):
+    batch_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+
+    # switch to evaluate mode
+    model.eval()
+
+    end = time.time()
+    for i, (input, target) in enumerate(testloader):
+        target = target.cuda(async=True)
+        input_var = torch.autograd.Variable(input, volatile=True)
+        target_var = torch.autograd.Variable(target, volatile=True)
+
+        # compute output
+        output, _= model(input_var)
+        loss = criterion(output, target_var)
+
+        # measure accuracy and record loss
+        prec1, prec5 = accuracy(output.data, target, topk=(1, 5))
+        losses.update(loss.data[0], input.size(0))
+        top1.update(prec1[0], input.size(0))
+        top5.update(prec5[0], input.size(0))
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % print_freq == 0:
+            print('Test: [{0}/{1}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+                  'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+                   i, len(val_loader), batch_time=batch_time, loss=losses,
+                   top1=top1, top5=top5))
+
+    print(' * Prec@1 {top1.avg:.3f} Prec@5 {top5.avg:.3f}'
+          .format(top1=top1, top5=top5))
+
+
+criterion = nn.CrossEntropyLoss().cuda()
+prec1 = validate(testloader, model, criterion)
