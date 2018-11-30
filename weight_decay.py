9da1d0afdb73b7019312ac7475e733c1c65dd59e
gngdb <gavingray1729@gmail.com>
2018-11-30 12:21:09 +0000

    log parameterisation

--- weight_decay.py
+++ weight_decay.py
@@ -6,10 +6,10 @@ from pytorch_acdc.layers import FastStackedConvACDC
 from tqdm import tqdm
 from collections import OrderedDict
 
-def substitute_jitter(l, sigma):
+def substitute_jitter(l, logsigma):
     new_sd = OrderedDict()
     for n,p in l.named_parameters():
-        new_sd[n] = torch.randn(p.size())*sigma
+        new_sd[n] = torch.randn(p.size())*torch.exp(logsigma)
     l.load_state_dict(new_sd)
     return new_sd
 
@@ -26,20 +26,20 @@ if __name__ == '__main__':
     N = 1000
     #X = torch.randn(5,4,32,32)
     l = FastStackedConvACDC(4,4,1,12,bias=False)
-    sigma = torch.Tensor([math.sqrt(2.)])
-    sigma.requires_grad = True
+    logsigma = torch.Tensor([math.sqrt(1.)])
+    logsigma.requires_grad = True
     l.eval()
-    optimizer = optim.SGD([sigma], lr=0.00001, momentum=0.9)
+    optimizer = optim.SGD([logsigma], lr=0.001, momentum=0.9)
     with tqdm(total=N) as pbar:
         for i in range(N):
             l.zero_grad()
-            if sigma.grad is not None:
-                sigma.grad.zero_()
-            substitutes = substitute_jitter(l, sigma)
+            if logsigma.grad is not None:
+                logsigma.grad.zero_()
+            substitutes = substitute_jitter(l, logsigma)
             W = l(X)
-            loss = F.mse_loss(W.var(),torch.Tensor([5e-4]))
+            loss = F.mse_loss(torch.log(W.var()),torch.log(torch.Tensor([5e-4])))
             loss.backward()
             torch.autograd.backward(substitutes.values(), grad_tensors=[p.grad for p in l.parameters()])
             optimizer.step()
             pbar.update(1)
-            pbar.set_description("Loss/Sigma: %f %f"%(loss.item(), sigma.item()))
+            pbar.set_description("Loss/Sigma: %f %f"%(loss.item(), torch.exp(logsigma).item()))
